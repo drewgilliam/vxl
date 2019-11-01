c537d7c5a4ba9b4495b61fb82a65a55ffb9635a9
Drew Gilliam <drew@visionsystemsinc.com>
2019-11-01 08:51:27 -0400

    more debug

--- contrib/brl/bbas/bpgl/algo/bpgl_gridding.h
+++ contrib/brl/bbas/bpgl/algo/bpgl_gridding.h
@@ -210,9 +210,13 @@ class linear_interp : public base_interp<T, DATA_T>
 
     // check reciprocal condition number
     auto rcond = inv_AtA_RtR.well_condition();
-    std::cout << "matrix condition (" << rcond << ")\n";
-    std::cout << "A =\n" << A << "\n";
-    std::cout << "inv_AtA_RtR =\n" << inv_AtA_RtR.as_matrix() << "\n";
+    std::cout << "matrix condition (" << rcond << ")\n"
+              << "A =\n" << A << "\n"
+              << "inv_AtA_RtR =\n" << inv_AtA_RtR.as_matrix() << "\n"
+              << "b =\n" << b << "\n"
+              << "origin (x,y,v) = " << x_origin << "," << y_origin << "," << v_origin << "\n"
+              ;
+
     if (rcond < rcond_thresh_) {
       std::cerr << "matrix has poor condition (" << rcond << ")\n" << std::endl;
       return this->invalid_val_;
